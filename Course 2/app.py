ab7863847a602fb264d4c86e2b4a4eace956aa6a
Rohith M <rohithmerala@gmail.com>
2025-04-06 09:21:52 +0800

    Can now detect

---
+++ Course 2/app.py
@@ -0,0 +1,73 @@
+import pyhula
+import time
+from hula_video import hula_video
+from tflite_detector import tflite_detector
+import cv2
+import numpy as np
+import threading
+import math
+
+api = pyhula.UserApi()
+
+move_complete = False
+
+
+def circle_up(radius, height_increase, num_steps, loops):
+    while _ < loops:
+        delta_theta = 2 * math.pi / num_steps
+        z = height_increase / num_steps
+
+        for i in range(num_steps):
+            theta_i = i * delta_theta
+            theta_next = (i + 1) * delta_theta
+
+            x_i = radius * math.cos(theta_i)
+            y_i = radius * math.sin(theta_i)
+            x_next = radius * math.cos(theta_next)
+            y_next = radius * math.sin(theta_next)
+
+            dx = x_next - x_i
+            dy = y_next - y_i
+
+            turn = 360/num_steps
+
+            api.single_fly_curvilinearFlight(dx, dy, z)
+            api.single_fly_turnleft(turn)
+
+            time.sleep(0.1)
+        _+=1
+
+    move_complete = True
+
+def detection():
+    while not move_complete:
+        frame = video.get_video()
+        object_found, frame = detector.detect(frame)
+        if not object_found is None:
+            print(F"Found object: {object_found}")
+        cv2.imshow("Detection", frame)
+        cv2.waitKey(1)
+        time.sleep(0.1)
+
+
+if not api.connect():
+    print("Connection error")
+else:
+    print("Connection to station by WiFi")
+    battery = api.get_battery()
+    print("battery level: " + str(api.get_battery()))
+
+    video = hula_video(hula_api=api,display=False)
+    detector = tflite_detector(model="model.tflite",label="label.txt")
+    video.video_mode_on()
+
+    api.single_fly_takeoff()
+
+    move = threading.Thread(target=circle_up, args=[50,20,100, 1])
+    detect = threading.Thread(target=detection, args=[])
+    move.start()
+    detect.start()
+    move.join()
+    detect.join()
+
+    api.single_fly_touchdown()
